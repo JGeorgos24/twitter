0e6e5d789375288a15755664c34a2f3d947680d2
georgosjohn <GeorgosJohn@JohnDeere.com>
2020-10-15 14:55:50 -0500

    initial setup complete

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
+// Twitter Clone
+// Components --> App--> Profile, News Feed, Individual tweet
+// state -> user, tweets, likes
+// user: {username, password, bio}
+// tweets: [{id,userid,context}]
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      user:{
+        id:1,
+        username:"CRHarding",
+        password: "casey123",
+        bio: "To be detemined"
+      },
+      tweets: []
+    }
+  }
+
+  render(){
+     return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> Twitter &copy; CRHarding </h1>
     </div>
-  );
+  ); 
+  }
+
 }
 
 export default App;
